7998afa3730e40c6c3e7fe1bfdfc91b10116a3a6
iliamalek <iliamalek@gmail.com>
2019-09-19 00:34:41 +0330

    Create showcat.aspx.cs

---
+++ showcat.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class showcats : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        DataSet_categoryTableAdapters.categoryTableAdapter cata = new DataSet_categoryTableAdapters.categoryTableAdapter();
+        string cid = Request.QueryString["cid"];
+        string TitleOfCat = cata.TitleOfCategory(int.Parse(cid)).ToString();
+        Page.Title = TitleOfCat;
+    }
+}
