8250df0938c196ea23e0f3bada48ab8efbc0954e
iliamalek <iliamalek@gmail.com>
2019-09-19 00:42:13 +0330

    Create category.aspx.cs

---
+++ Admin/category.aspx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Admin_category : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void btn_cat_Click(object sender, EventArgs e)
+    {
+        DataSet_categoryTableAdapters.categoryTableAdapter cta = new DataSet_categoryTableAdapters.categoryTableAdapter();
+        cta.Insert_category(txt_catname.Text);
+        lbl_cat.Text = "موضوع با موفقیت ثبت گردید";
+        GridView1.DataBind();
+        txt_catname.Text = "";
+    }
+}
