4fbe7ed9b02982323d4d9a58e6d7d78b752269d5
iliamalek <iliamalek@gmail.com>
2019-09-19 00:41:43 +0330

    Create Link.aspx.cs

---
+++ Admin/Link.aspx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Admin_Link : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void btn_link_Click(object sender, EventArgs e)
+    {
+        DataSet_linkTableAdapters.tbl_linkTableAdapter lta = new DataSet_linkTableAdapters.tbl_linkTableAdapter();
+        lta.InsertLink(txt_title.Text, txt_url.Text, txt_desc.Text);
+        lbl_link.Text = "لینک جدید ثبت گردید";
+        GridView1.DataBind();
+        txt_title.Text = "";
+        txt_url.Text = "";
+        txt_desc.Text = "";
+    }
+}
